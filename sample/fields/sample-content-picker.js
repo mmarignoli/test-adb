0b0a99ae34a2a4ccbfe1af22a7d39e49dcbe711f
marcostorto <marcostorto@users.noreply.github.com>
2016-09-26 09:30:51 +0200

    Update sample-content-picker.js

--- sample/fields/sample-content-picker.js
+++ sample/fields/sample-content-picker.js
@@ -30,13 +30,7 @@ define(function(require, exports, module) {
                         "_type": this.options.contentType
                     },
                     "mappings": {
-                        "value": {
-                            "id": "_doc",
-                            "ref": "node://824bb3e46ebd8c712033/015edf632778e2914d4d/8e7a294fcdced19898ae/",
-                            "title": "title"
-                            "qname": "_qname",
-                            "typeQName": "_type"
-                        },
+                        "value": "_doc",
                         "text": "title"
                     }
                 }
