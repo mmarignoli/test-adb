c060601b21af6ade8c67570623a6dc53533031d2
marcostorto <marcostorto@users.noreply.github.com>
2017-02-07 00:13:58 +0100

    Update command.js

--- sample/fields/command.js
+++ sample/fields/command.js
@@ -15,6 +15,60 @@ define(function(require, exports, module) {
      */
     return UI.registerField("command", Alpaca.Fields.ObjectField.extend({
 
+        setup: function()
+        {
+            this.base();
+
+            if (!this.data || !this.data.street) {
+                this.data = {
+                    street: ['', '']
+                };
+            }
+
+            this.schema = {
+                "title": "Home Address",
+                "type": "object",
+                "properties": {
+                    "street": {
+                        "title": "Street",
+                        "type": "array",
+                        "items": {
+                            "type": "string",
+                            "maxLength": 30
+                        },
+                        "minItems": 0,
+                        "maxItems": 3
+                    },
+                    "city": {
+                        "title": "City",
+                        "type": "string"
+                    },
+                    "state": {
+                        "title": "State",
+                        "type": "string",
+                        "enum": ["AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FM", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MH", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PW", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI", "VA", "WA", "WV", "WI", "WY"]
+                    },
+                    "zip": {
+                        "title": "Zip Code",
+                        "type": "string",
+                        "pattern": /^(\d{5}(-\d{4})?)?$/
+                    }
+                }
+            };
+            Alpaca.merge(this.options, {
+                "fields": {
+                    "zip": {
+                        "maskString": "99999",
+                        "size": 5
+                    },
+                    "state": {
+                        "optionLabels": ["ALABAMA", "ALASKA", "AMERICAN SAMOA", "ARIZONA", "ARKANSAS", "CALIFORNIA", "COLORADO", "CONNECTICUT", "DELAWARE", "DISTRICT OF COLUMBIA", "FEDERATED STATES OF MICRONESIA", "FLORIDA", "GEORGIA", "GUAM", "HAWAII", "IDAHO", "ILLINOIS", "INDIANA", "IOWA", "KANSAS", "KENTUCKY", "LOUISIANA", "MAINE", "MARSHALL ISLANDS", "MARYLAND", "MASSACHUSETTS", "MICHIGAN", "MINNESOTA", "MISSISSIPPI", "MISSOURI", "MONTANA", "NEBRASKA", "NEVADA", "NEW HAMPSHIRE", "NEW JERSEY", "NEW MEXICO", "NEW YORK", "NORTH CAROLINA", "NORTH DAKOTA", "NORTHERN MARIANA ISLANDS", "OHIO", "OKLAHOMA", "OREGON", "PALAU", "PENNSYLVANIA", "PUERTO RICO", "RHODE ISLAND", "SOUTH CAROLINA", "SOUTH DAKOTA", "TENNESSEE", "TEXAS", "UTAH", "VERMONT", "VIRGIN ISLANDS", "VIRGINIA", "WASHINGTON", "WEST VIRGINIA", "WISCONSIN", "WYOMING"]
+                    }
+                }
+            });
+            this.base();
+        }
+            
         getFieldType: function() {
             return "custom";
         },
@@ -26,6 +80,7 @@ define(function(require, exports, module) {
                 deviceCommand.on("change", function() {
                     var deviceCommandParameterInstances = self.childrenByPropertyId["deviceCommandParameterInstances"];
                     var successCallback = function(obj) {
+                        deviceCommandParameterInstances.data = obj.deviceCommandParameterInstances;
                         deviceCommandParameterInstances.setValue(obj.deviceCommandParameterInstances);
                     }
                     var nodeId = this.data;
@@ -43,12 +98,6 @@ define(function(require, exports, module) {
                 callback();
 
             });
-        },
-        
-        setup: function () {
-
-            this.base();
-
         }
 
     }));
