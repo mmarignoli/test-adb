c20be5292217f8b1982c61de2a1fbec43e8049bc
marcostorto <marcostorto@users.noreply.github.com>
2017-02-22 18:07:31 +0100

    Update array-numbered.js

--- sample/fields/array-numbered.js
+++ sample/fields/array-numbered.js
@@ -3,7 +3,7 @@ define(function(require, exports, module) {
     var UI = require("ui");
     var Alpaca = require("alpaca");
 
-    return UI.registerField("layout-object", Alpaca.Fields.ArrayField.extend({
+    return UI.registerField("array-numbered", Alpaca.Fields.ArrayField.extend({
        
         createItem: function(index, itemSchema, itemOptions, itemData, postRenderCallback)
         {
