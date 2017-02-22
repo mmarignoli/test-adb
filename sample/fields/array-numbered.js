aac85118112656c930d5d5427d6eb7611f91decd
marcostorto <marcostorto@users.noreply.github.com>
2017-02-22 18:04:19 +0100

    Update array-numbered.js

--- sample/fields/array-numbered.js
+++ sample/fields/array-numbered.js
@@ -10,39 +10,7 @@ define(function(require, exports, module) {
             itemOptions.label = itemOptions.label + " " + index + " ";
             this.base(index, itemSchema, itemOptions, itemData, postRenderCallback);
         }
-        
-        /**
-         * @see Alpaca.ControlField#getFieldType
-         */
-        getFieldType: function() {
-            return "array";
-        },
 
-        /**
-         * @see Alpaca.ControlField#getType
-         */
-        getType: function() {
-            return "array";
-        }
-
-        /* builder_helpers */
-        ,
-
-        /**
-         * @see Alpaca.ControlField#getTitle
-         */
-        getTitle: function() {
-            return "Layout object Field";
-        },
-
-        /**
-         * @see Alpaca.ControlField#getDescription
-         */
-        getDescription: function() {
-            return "Renders object items into a layout";
-        }
-
-        /* end_builder_helpers */
     }));
 
 });
